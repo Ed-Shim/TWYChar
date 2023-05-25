13044a23570264ebeb27c87682f78301655daaf4
Ed-Shim <107801628+Ed-Shim@users.noreply.github.com>
2023-05-25 09:09:05 +0000

    Merged frontend and backend so anyone can use the product. Updated UI.

--- src/ChatRoom.jsx
+++ src/ChatRoom.jsx
@@ -1,37 +1,80 @@
-// ChatRoom.jsx
-import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Box, Typography, Button, Avatar } from "@mui/material";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-// Add other imports as necessary...
+import IconButton from "@mui/material/IconButton";
+import SendIcon from "@mui/icons-material/Send";
+import { useNavigate, useLocation } from 'react-router-dom';
 
-const ChatRoom = () => {
-  const [messages, setMessages] = useState(["Thanks for creating me!"]);
+const ChatRoom = ({ botPicture }) => {
+  const [messages, setMessages] = useState([{ sender: 'bot', text: 'Hello, how can I help you?' }]);
   const [newMessage, setNewMessage] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
+  const botIcon = location.state.botIcon;
 
   const handleSendMessage = () => {
-    setMessages([...messages, newMessage]);
-    setNewMessage("");
+    if (newMessage.trim()) {
+      setMessages([...messages, { sender: 'user', text: newMessage }]);
+      setNewMessage("");
+    }
   };
 
-  const handleLeaveRoom = () => {
-    navigate("/");
+  // Mimic bot responses for the demo
+  useEffect(() => {
+    if (messages.length && messages[messages.length - 1].sender !== 'bot') {
+      setTimeout(() => {
+        setMessages([...messages, { sender: 'bot', text: 'Sorry, I am not capable of real conversation yet.' }]);
+      }, 1000);
+    }
+  }, [messages]);
+
+  const closeConversation = () => {
+    navigate('/');
   };
+
 
   return (
-    <div>
-      <Button onClick={handleLeaveRoom}>Leave Room</Button>
-      <div>
-        {messages.map((message, index) => (
-          <Typography key={index}>{message}</Typography>
+    <Box sx={{ height: 'calc(100vh - 64px)', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+      <Box sx={{ overflowY: 'scroll' }}>
+        {messages.map((message, i) => (
+          <Box key={i} sx={{ display: 'flex', justifyContent: message.sender === 'user' ? 'flex-end' : 'flex-start', alignItems: 'center' }}>
+            {message.sender === 'bot' && <Avatar src={botPicture} sx={{ margin: 2 }} />}
+            <Box
+              sx={{
+                padding: 2,
+                margin: 2,
+                borderRadius: 2,
+                backgroundColor: message.sender === 'user' ? '#3f51b5' : '#e0e0e0',
+                color: message.sender === 'user' ? '#ffffff' : '#000000',
+                maxWidth: '60%',
+                wordBreak: 'break-word'
+              }}
+            >
+              <Typography variant="body1">{message.text}</Typography>
+            </Box>
+          </Box>
         ))}
-      </div>
-      <TextField value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
-      <Button onClick={handleSendMessage}>Send</Button>
-    </div>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', padding: 2, borderTop: '1px solid #ddd', position: 'sticky', bottom: 0, backgroundColor: '#fff' }}>
+        <TextField
+          fullWidth
+          placeholder="Type a message..."
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              handleSendMessage();
+            }
+          }}
+        />
+        <IconButton onClick={handleSendMessage}>
+          <SendIcon />
+        </IconButton>
+      </Box>
+      <Box sx={{ padding: 2 }}>
+        <Button variant="outlined" color="secondary" onClick={closeConversation}>Close Conversation</Button>
+      </Box>
+    </Box>
   );
 };
 
--- src/CreateCharacter.jsx
+++ src/CreateCharacter.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
-import { Container, Grid, TextField, Button, Typography, Box, CssBaseline, useMediaQuery, createTheme, ThemeProvider } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Container, Grid, TextField, Button, Typography, Box, CssBaseline, useMediaQuery, createTheme, ThemeProvider, Chip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import "./App.css"; // CSS Styles
 
 const CreateCharacter = () => {
-  const [characterSettings, setCharacterSettings] = useState([]);
-  const [sampleResponses, setSampleResponses] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
+  // Load characterSettings, sampleResponses, and selectedImage from local storage if they exist
+  const initialCharacterSettings = JSON.parse(localStorage.getItem('characterSettings')) || [];
+  const initialSampleResponses = JSON.parse(localStorage.getItem('sampleResponses')) || [];
+  const initialSelectedImage = localStorage.getItem('selectedImage') || null;
+
+  const [characterSettings, setCharacterSettings] = useState(initialCharacterSettings);
+  const [sampleResponses, setSampleResponses] = useState(initialSampleResponses);
+  const [selectedImage, setSelectedImage] = useState(initialSelectedImage);
   const [newSetting, setNewSetting] = useState("");
   const [newResponse, setNewResponse] = useState("");
   const [apiKey, setApiKey] = useState(""); // state for the API key
+  const [apiKeyError, setApiKeyError] = useState(false);
   const navigate = useNavigate();
 
   const theme = createTheme({
@@ -32,31 +38,68 @@ const CreateCharacter = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-
-    const formData = new FormData();
-    formData.append("settings", JSON.stringify(characterSettings));
-    formData.append("responses", JSON.stringify(sampleResponses));
-    formData.append("apiKey", apiKey);
-    if (selectedImage) formData.append("image", selectedImage, selectedImage.name);
-
-    try {
-      const result = await fetch("https://character-server.eisukeshimizu.repl.co/create-chatbot", {
-        method: "POST",
-        body: formData,
-      });
-
-      const body = await result.json();
-      console.log(body);
-      setNewSetting("");
-      setNewResponse("");
-      navigate("/chatroom");
-    } catch (error) {
-      console.log(error);
+    if (apiKey === "") {
+      setApiKeyError(true);
+      return;
     }
+
+    setApiKeyError(false);
+
+    // Construct the prompt for the OpenAI API based on the user input
+    const prompt = `The following is the background of the character: ${characterSettings.join('. ')}\n Some samples of response by the character is as follows: ${sampleResponses.join('\n')}`;
+    console.log("Submitted " + prompt);
+
+    // const response = await openai.createChatCompletion({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [{
+    //       role: "system", content: prompt},
+    //     { role: "user", content: message }],
+    //     temperature: 0.5,
+    //     max_tokens: 500
+    //   });
+
+    // Clear inputs
+    setCharacterSettings([]);
+    setSampleResponses([]);
+    setSelectedImage(null);
+    setNewSetting("");
+    setNewResponse("");
+    navigate("/chatroom", { state: { botIcon: selectedImage } });
   };
 
   const handleImageUpload = (e) => {
-    setSelectedImage(e.target.files[0]);
+    const file = e.target.files[0];
+    const maxSizeInBytes = 5 * 1024 * 1024; // 5 MB
+    if (file.size > maxSizeInBytes) {
+      alert('The file is too large. Please select a file that is less than 5MB.');
+      return;
+    }
+    const reader = new FileReader();
+    reader.onloadend = function() {
+      setSelectedImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+
+  useEffect(() => {
+    localStorage.setItem('characterSettings', JSON.stringify(characterSettings));
+    localStorage.setItem('sampleResponses', JSON.stringify(sampleResponses));
+    localStorage.setItem('selectedImage', selectedImage);
+  }, [characterSettings, sampleResponses, selectedImage]);
+
+  const handleReset = () => {
+    // Reset state and remove from local storage
+    setCharacterSettings([]);
+    setSampleResponses([]);
+    setSelectedImage(null);
+    setNewSetting("");
+    setNewResponse("");
+    setApiKey("");
+    setApiKeyError(false);
+    localStorage.removeItem('characterSettings');
+    localStorage.removeItem('sampleResponses');
+    localStorage.removeItem('selectedImage');
   };
 
   const totalCharacters = [...characterSettings, ...sampleResponses].join().length;
@@ -70,41 +113,46 @@ const CreateCharacter = () => {
         </Typography>
         <form onSubmit={handleSubmit} noValidate autoComplete="off">
           <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <Typography variant="h6" component="h2" gutterBottom color="secondary">
+            <Grid item xs={12}>
+              <Typography variant="h6" component="h2" gutterBottom color="textSecondary">
                 Upload Image
               </Typography>
               <Button variant="contained" component="label" color="primary" fullWidth>
                 Upload Image
                 <input type="file" hidden onChange={handleImageUpload} />
               </Button>
-              {selectedImage ? (
-                <img src={URL.createObjectURL(selectedImage)} alt="Character" style={{ width: '100%', marginTop: theme.spacing(2) }} />
-              ) : (
+              {selectedImage && (
+                <img src={selectedImage} alt="Character" style={{ maxWidth: '100%', height: 'auto', marginTop: theme.spacing(2) }} />
+              )}
+              {!selectedImage && (
                 <Typography color="textSecondary" style={{ marginTop: theme.spacing(2) }}>
                   No image selected
                 </Typography>
               )}
+
+
             </Grid>
             <Grid item xs={12} md={6}>
-              <Typography variant="h2">Character Settings</Typography>
-              {characterSettings.map((setting, index) => (
-                <Box key={index} display="flex" justifyContent="space-between">
-                  <Typography>{setting}</Typography>
-                  <Button variant="outlined" color="secondary" onClick={() => setCharacterSettings(characterSettings.filter((_, i) => i !== index))}>X</Button>
-                </Box>
-              ))}
+              <Typography variant="h6" color="textSecondary">Character Settings</Typography>
+              <Box display="flex" flexWrap="wrap" sx={{ minHeight: '100px', backgroundColor: '#333', padding: 2, borderRadius: 2 }}>
+                {characterSettings.map((setting, index) => (
+                  <Box key={index} sx={{ margin: 1 }}>
+                    <Chip label={setting} onDelete={() => setCharacterSettings(characterSettings.filter((_, i) => i !== index))} style={{ backgroundColor: '#fff', color: '#808080' }} />
+                  </Box>
+                ))}
+              </Box>
               <TextField label="New setting" value={newSetting} onChange={(e) => setNewSetting(e.target.value)} />
               <Button variant="contained" color="primary" onClick={() => { setCharacterSettings([...characterSettings, newSetting]); setNewSetting(""); }}>Add Setting</Button>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h2">Sample Responses</Typography>
-              {sampleResponses.map((response, index) => (
-                <Box key={index} display="flex" justifyContent="space-between">
-                  <Typography>{response}</Typography>
-                  <Button variant="outlined" color="secondary" onClick={() => setSampleResponses(sampleResponses.filter((_, i) => i !== index))}>X</Button>
-                </Box>
-              ))}
+            <Grid item xs={12} md={6}>
+              <Typography variant="h6" color="textSecondary">Sample Responses</Typography>
+              <Box display="flex" flexWrap="wrap" sx={{ minHeight: '100px', backgroundColor: '#333', padding: 2, borderRadius: 2 }}>
+                {sampleResponses.map((response, index) => (
+                  <Box key={index} sx={{ margin: 1 }}>
+                    <Chip label={response} onDelete={() => setSampleResponses(sampleResponses.filter((_, i) => i !== index))} style={{ backgroundColor: '#fff', color: '#808080' }} />
+                  </Box>
+                ))}
+              </Box>
               <TextField label="New response" value={newResponse} onChange={(e) => setNewResponse(e.target.value)} />
               <Button variant="contained" color="primary" onClick={() => { setSampleResponses([...sampleResponses, newResponse]); setNewResponse(""); }}>Add Response</Button>
             </Grid>
@@ -112,14 +160,23 @@ const CreateCharacter = () => {
               <TextField
                 label="API Key"
                 value={apiKey}
-                onChange={(e) => setApiKey(e.target.value)}
+                onChange={(e) => {
+                  setApiKey(e.target.value);
+                  setApiKeyError(false);
+                }}
               />
+
             </Grid>
 
             <Grid item xs={12}>
-
               <Button variant="contained" color="primary" type="submit">Create Chat Bot</Button>
+              <Button variant="contained" color="secondary" onClick={handleReset} style={{ marginLeft: '20px' }}>Reset</Button>
               <Typography variant="subtitle1">{`Total characters: ${characterSettings.join('').length + sampleResponses.join('').length}`}</Typography>
+              {apiKeyError && (
+                <Typography color="error">
+                  Please add an API key to create the bot.
+                </Typography>
+              )}
             </Grid>
           </Grid>
         </form>
